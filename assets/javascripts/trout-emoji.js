15adf327c8b12b7f6ac843abd6b0fdeaccb81da9
John Parsons <djshaneypuppro@gmail.com>
2015-08-10 03:49:51 -0400

    Testing edits 1
    Added Discourse.Dialect.registerEmoji('Kappa',
    'https://static-cdn.jtvnw.net/emoticons/v1/25/1.0'); to trout-emoji.js

--- assets/javascripts/trout-emoji.js
+++ assets/javascripts/trout-emoji.js
@@ -1,3 +1,4 @@
 (function() {
   Discourse.Dialect.registerEmoji('trout', '/plugins/trout_emoji/images/trout-square.jpg');
+  Discourse.Dialect.registerEmoji('Kappa', 'https://static-cdn.jtvnw.net/emoticons/v1/25/1.0');
 })();
