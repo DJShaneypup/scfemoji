e02f0a23d45f815c2fed2ae932e0df76380dd1e9
John Parsons <djshaneypuppro@gmail.com>
2015-08-11 19:57:37 -0400

    0.2.3
    fixed plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,19 @@
-# name: trout_emoji_edit_SCEMOJI
+# name: SCEMOJI
 # about: adds an evil trout emoji
-# version: 0.2
-# authors: Robin Ward, DJShaneypup, DanTheDak
+# version: 0.2.3
+# authors: DJShaneypup, DanTheDak
 # url: https://github.com/DJShaneypup/scfemoji
 
-register_asset "javascripts/trout-emoji.js", :server_side
+register_asset "javascripts/biblethump.js", :server_side
+register_asset "javascripts/coolcat.js", :server_side
+register_asset "javascripts/datsheffy.js", :server_side
+register_asset "javascripts/deilluminati.js", :server_side
+register_asset "javascripts/frankerz.js", :server_side
+register_asset "javascripts/kapow.js", :server_side
+register_asset "javascripts/kappa.js", :server_side
+register_asset "javascripts/kappapride.js", :server_side
+register_asset "javascripts/manload.js", :server_side
+register_asset "javascripts/pjsalt.js", :server_side
+register_asset "javascripts/shibez.js", :server_side
+register_asset "javascripts/vaultboy.js", :server_side
+
